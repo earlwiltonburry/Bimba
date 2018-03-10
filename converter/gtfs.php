aa22149d0c7ee91a66b0bb82baebb740fe3843cc
Adam Pioterek <adam.pioterek@protonmail.ch>
2018-03-10 20:27:43 +0100

    gtfs in sqlite download facade

---
+++ converter/gtfs.php
@@ -0,0 +1,32 @@
+<?php
+
+require 'vendor/mustangostang/spyc/Spyc.php';
+
+$metadata = Spyc::YAMLLoad('metadata.yml');
+$today = date('Ymd');
+$current = '';
+$sizeU = '';
+$sizeC = '';
+
+foreach($metadata as $row) {
+    $start = $row['start'];
+    $end = $row['end'];
+    if ($start <= $today and $today <= $end) {
+        $current = $row['id'];
+        $sizeU = $row['size_uncompressed'];
+        $sizeC = $row['size_compressed'];
+        break;
+    }
+}
+unset($row);
+
+$etag = $_SERVER['HTTP_ETAG'];
+
+if ($etag == $current) {
+    http_response_code(304);
+} else {
+    header("ETag: $current");
+    // todo send file
+    echo $current;
+}
+?>
